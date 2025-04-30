d6038b4f0afbbf1dce0bdc870c867c22ad21864c
L2W214 <66002146+L2W214@users.noreply.github.com>
2025-04-30 10:11:53 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,79 @@
+from model import *
+from dataset import *
+
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+import torch.utils.data
+
+
+def main():
+    device = torch.device("cuda:0")
+
+    collect_fun = SparseCollation(voxel_size=0.07)
+
+    train_dataset = S3DIS()
+    test_dataset = S3DIS('test')
+
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=8, collate_fn=collect_fun, shuffle=True,
+                                                   num_workers=8)
+    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=8, collate_fn=collect_fun, shuffle=True,
+                                                  num_workers=8)
+
+    model = OursModel(6, is_cat=True).to(device)
+    model_head = SegmentationClassifierHead(32, 13).to(device)
+
+    model.train()
+    model_head.train()
+
+    criterion = torch.nn.CrossEntropyLoss()
+    optim_params = list(model.parameters()) + list(model_head.parameters())
+
+    optimizer = torch.optim.SGD(optim_params, lr=2.4e-2, momentum=0.9, weight_decay=1.0e-4)
+    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.95)
+
+    acc_list, loss_list = [], []
+    for _ in tqdm(range(100), total=100):
+        loss_sum = 0
+        accuracy_sum = 0
+        for data in train_dataloader:
+            coord, feats, sem_label = data
+            sem_label = sem_label.to(device).long()
+            coord = ME.SparseTensor(feats.float(), coord, device=device)
+
+            optimizer.zero_grad()
+
+            coord = model(coord)
+            predict_label = model_head(coord)
+
+            loss = criterion(predict_label, sem_label.long())
+            loss.backward()
+
+            optimizer.step()
+
+            predict_choice = predict_label.data.max(dim=1)[1]
+            correct = predict_choice.eq(sem_label.data).cpu().sum()
+
+            loss_sum += loss.detach().cpu()
+            accuracy_sum += correct.item() / float(sem_label.shape[0])
+
+        scheduler.step()
+
+        loss_mean = loss_sum / len(train_dataloader)
+        loss_list.append(loss_mean)
+
+        acc_mean = accuracy_sum / len(train_dataloader)
+        acc_list.append(acc_mean)
+
+    torch.save(model.state_dict(), 'model.pth')
+    torch.save(model_head.state_dict(), 'model_head.pth')
+
+    plt.plot(loss_list, 'r', label='LOSS_CURVE')
+    plt.plot(acc_list, 'b', label='ACCURACY_CURVE')
+    plt.legend()
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
